6489edc58bdf73bac0242415781dc3ffdc83d8e6
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2024-05-15 21:27:07 +0100

    Revert "HW2"
    This reverts commit 7678611bedfe39d9a2a9fe6b62942cb27eb1aef6.

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,9 +1,7 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import user from "./user";
-import book from "./books";
+import user from './user';
 
 export default combineReducers({
   user,
-  book,
 });
