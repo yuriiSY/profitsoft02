4c8d982d47dbca6af9dae152ea3549aa89011606
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2024-05-15 22:22:08 +0100

    ж

--- src/pages/default/index.jsx
+++ src/pages/default/index.jsx
@@ -1,12 +1,14 @@
-import React, { useMemo } from "react";
-import IntlProvider from "misc/providers/IntlProvider";
-import useLocationSearch from "misc/hooks/useLocationSearch";
+import React, { useMemo } from 'react';
+import IntlProvider from 'misc/providers/IntlProvider';
+import useLocationSearch from 'misc/hooks/useLocationSearch';
 
-import getMessages from "./intl";
-import Default from "./containers/Default";
+import getMessages from './intl';
+import Default from './containers/Default';
 
 function Index(props) {
-  const { lang } = useLocationSearch();
+  const {
+    lang,
+  } = useLocationSearch();
   const messages = useMemo(() => getMessages(lang), [lang]);
   return (
     <IntlProvider messages={messages}>
