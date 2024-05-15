35bd01437b9b93eff7523261efc6ce788e5f773e
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2024-05-15 21:40:36 +0100

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "HW2"""""""
    This reverts commit 675ff3cf03fc010a6e5c6cae52d35114f4d75521.

--- src/pages/secret/containers/Secret.jsx
+++ src/pages/secret/containers/Secret.jsx
@@ -1,23 +1,14 @@
-import { useIntl } from "react-intl";
-import css from "./Secret.module.css";
-import React from "react";
-import Typography from "components/Typography";
-import CardList from "../components/CardList/CardList";
-import { Link } from "react-router-dom";
+import { useIntl } from 'react-intl';
+import React from 'react';
+import Typography from 'components/Typography';
 
 function Secret() {
   const { formatMessage } = useIntl();
 
   return (
-    <>
-      <Typography>{formatMessage({ id: "title" })}</Typography>
-      <Link to="/addBook">
-        <button className={css.addBtn}>
-          {formatMessage({ id: "button" })}
-        </button>
-      </Link>
-      <CardList />
-    </>
+    <Typography>
+      {formatMessage({ id: 'title' })}
+    </Typography>
   );
 }
 
